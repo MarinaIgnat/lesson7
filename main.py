5f2dd6f44e3387b6ff17f50d770a623977f1b685
MarinaIgnat <9702m@mail.ru>
2024-07-27 17:24:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5
 
 students_new = sorted(students)
 
-def average(*args): print([sum(x) / len(x) for x in args])
-grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
-average(*grades) #как мне здесь присвоить имя строки?
 
-result = dict(zip(students_new, average(*grades)))
+def average(*args):
+    return [sum(x) / len(x) for x in args]
+
+
+average_grades = average(*grades)
+
+result = dict(zip(students_new, average_grades))
 print(result)
